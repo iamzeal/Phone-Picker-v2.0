c0b7582bb2dd1b0cfc7370d2c710094a72e760de
Shikhar Shrivastava <Shikhar Shrivastava>
2017-03-04 23:24:35 +0530

    integrating wit

--- app.js
+++ app.js
@@ -98,6 +98,7 @@ const findOrCreateSession = (fbid) => {
 
 // Our bot actions
 const actions = {
+  const sessionId = 1111;
   send({sessionId}, {text}) {
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
