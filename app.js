07a9464ed921b1623e46d1d9202c303a47fa1f85
Shikhar Shrivastava <Shikhar Shrivastava>
2017-03-05 11:29:26 +0530

    added getForecast

--- app.js
+++ app.js
@@ -123,6 +123,19 @@ const actions = {
       return Promise.resolve()
     }
   },
+   getForecast({context, entities}) {
+  return new Promise(function(resolve, reject) {
+    var location = firstEntityValue(entities, "location")
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return resolve(context);
+    });
+  },
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
 };
