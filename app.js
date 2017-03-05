339bd5337f32257f7252c9ed7d08e721bfedbb59
Shikhar Shrivastava <Shikhar Shrivastava>
2017-03-05 12:01:39 +0530

    added time story method

--- app.js
+++ app.js
@@ -135,11 +135,24 @@ const actions = {
       return Promise.resolve()
     }
   },
-   getForecast({context, entities}) {
+  getForecast({context, entities}) {
   return new Promise(function(resolve, reject) {
     var location = firstEntityValue(entities, "location")
     if (location) {
       context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return resolve(context);
+    });
+  },
+  getTime({context, entities}) {
+  return new Promise(function(resolve, reject) {
+    var location = firstEntityValue(entities, "location")
+    if (location) {
+      context.forecast = getTime() + location; // we should call a weather API here
       delete context.missingLocation;
     } else {
       context.missingLocation = true;
